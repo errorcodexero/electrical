5fc6bc50608b2a860dfc6c065841e8712455ee87
Software3 <1425sw@gmail.com>
2018-01-25 20:44:50 -0800

    Add pwm test files for reading and writing between two arduinos

---
+++ pwm_testing/pwm_reader/pwm_reader.ino
@@ -0,0 +1,20 @@
+unsigned input_pin = 9;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(input_pin, INPUT);
+}
+
+double calculate_duty_cycle(unsigned input_pin){//reads the pwm value of an input
+  unsigned long high_time = pulseIn(input_pin, HIGH);
+  unsigned long low_time = pulseIn(input_pin, LOW);
+  unsigned long cycle_time = high_time  + low_time;
+  double duty_cycle = (double)high_time / (double)cycle_time;
+  return duty_cycle;
+}
+
+void loop(){
+  double duty_cycle = calculate_duty_cycle(input_pin);
+  double value = duty_cycle * 255;// multiply by 255 since analogWrite maps a 0-255 value to a 0-1
+  Serial.println(value);
+}
---
+++ pwm_testing/pwm_writer/pwm_writer.ino
@@ -0,0 +1,12 @@
+unsigned output_pin = 9;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+    for(int output = 0; output < 256; output++){ 
+      analogWrite(output_pin, output);//writes a 0-255 int to a pwm signal  
+      delay(100);
+    } 
+}
