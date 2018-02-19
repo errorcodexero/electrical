effef79d8804a6920c6c6267c165ce700c21a14e
Software3 <1425sw@gmail.com>
2018-02-19 14:54:22 -0800

    Add configuration for more consistent behavior of custom library management bash script

--- frc2018_blinky_lights/update.sh
+++ frc2018_blinky_lights/update.sh
@@ -2,17 +2,34 @@
 
 LIBRARY_NAME=Gong
 LIBARY_SOURCE_PATH=../libraries/
-SKETCHBOOK_PATH=/cygdrive/d/Libraries/Documents/Arduino/libraries
 
-if [ ! -d $LIBRARY_SOURCE_PATH/$LIBARY_NAME ] #check to see if the updated library exists
+CONF_FILE=gong.conf
+
+if [ ! -f $CONF_FILE ] #check to see if config file was read properly
 then
-	echo "Error: libary source path \" $LIBRARY_SOURCE_PATH/$LIBRARY_NAME \" does not exist"
+	
+	printf "No config file found. Creating \"$CONF_FILE\". Please populate."
+	printf "#text The second line in this file is the path to the Arduino sketchbook / extra libraries\n\n" > $CONF_FILE
+	exit 1
+fi	
+
+CONF_FILE_SKETCHBOOK_PATH_LINE=2
+SKETCHBOOK_PATH=$(sed -n ${CONF_FILE_SKETCHBOOK_PATH_LINE}p ${CONF_FILE}) 
+
+if [ -z "$SKETCHBOOK_PATH" ] #check to see if config file was read properly
+then
+	printf "Error: config file not formatted properly: target line $CONF_FILE_SKETCHBOOK_PATH_LINE is blank"
+	exit 1
+fi
+if [ ! -d $LIBRARY_SOURCE_PATH/$LIBARY_NAME ] #check to see if the updated library to copy exists
+then
+	printf "Error: libary source path \"$LIBRARY_SOURCE_PATH/$LIBRARY_NAME\" does not exist"
 	exit 1
 fi
 
 if [ ! -d $SKETCHBOOK_PATH ]  #check if the target directory exits
 then
-	echo "Error: path \" $SKETCHBOOK_PATH \" does not exist"
+	printf "Error: path \"$SKETCHBOOK_PATH\" does not exist"
 	exit 1
 fi
 
@@ -23,4 +40,4 @@ fi
 
 cp -r ../libraries/$LIBRARY_NAME $SKETCHBOOK_PATH/$LIBARY_NAME #update the library
 
-echo "Update successful"
+printf "Update successful"
