5b5dc828bad59c5ba1bb2d0d027ee5c0d69fab20
Error Code Xero - Electrical <1425electronics@gmail.com>
2018-02-18 16:09:36 -0800

    Work on pwm input from roborio

--- pwm_in_from_roborio/Roborio_Reading_Tests/Roborio_Reading_Tests.ino
+++ pwm_in_from_roborio/Roborio_Reading_Tests/Roborio_Reading_Tests.ino
@@ -1,9 +1,10 @@
-//Is paired with roboRIO and reads pwm values
 
-#include <assert.h>
+//Is paired with roboRIO and reads pwm values to pick a blinky lights mode
 
-static const byte PWM_PIN = 6;
-static const int DECIMALS_TO_KEEP = 1;
+#include "assert.h"
+
+const int PWM_PIN = 5; // the PWM input pin number
+const int DECIMALS_TO_KEEP = 1; //the number of decimals to keep when the value is scaled up and converted to an integer
  
 void setup() {
   pinMode(PWM_PIN, INPUT);
@@ -13,13 +14,10 @@ void setup() {
 void loop() {
   assert(DECIMALS_TO_KEEP > 0);
   double pwm_value = pulseIn(PWM_PIN, HIGH);
-  double value = (pwm_value - 1504)/504; //converts pwm to a number between -1.0 and 1.0
-  double scaled = round(value * pow(10,DECIMALS_TO_KEEP ));
-  Serial.println(scaled);
- // Serial.print("          ");
-  //Serial.print(duration);
-  //Serial.print("          ");
- // Serial.print(rio);
-  //Serial.print("          ");
- // Serial.println(dig);
+  double value = (pwm_value - 1504.0) / 504.0; //converts values we want to a number between -1.0 and 1.0
+  int scaled = (int)round(value * pow(10,DECIMALS_TO_KEEP ));
+  const int DISABLED_THRESHOLD = 1; //if the value is greater than this, then the robot's disabled
+  bool disabled = scaled  > DISABLED_THRESHOLD;
+  Serial.println((String)pwm_value + "   " + (String)value + "   " + (String)scaled);
 }
+
