690a568447dea7cef5a2724cff431d8b79ea9d30
Software3 <1425sw@gmail.com>
2018-03-15 20:57:18 -0700

    Add conner's breadboard testing

---
+++ misc/conner_testing/conner_testing.ino
@@ -0,0 +1,18 @@
+void setup() {
+	pinMode(8,OUTPUT);
+	pinMode(7,OUTPUT);
+	pinMode(6,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+	digitalWrite(8,HIGH);
+	digitalWrite(6,LOW);
+	delay(100);
+	digitalWrite(8,LOW);
+	digitalWrite(7,HIGH);
+	delay(100);
+	digitalWrite(6,HIGH);
+	digitalWrite(7,LOW);
+	delay(100);
+}
