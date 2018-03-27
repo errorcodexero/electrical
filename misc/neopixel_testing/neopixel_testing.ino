f4dfe130e5929688470aef7f0cfa405763fe79b4
Software3 <1425sw@gmail.com>
2018-03-26 19:01:03 -0700

    Update neopixel code for new driver station signal

--- misc/neopixel_testing/neopixel_testing.ino
+++ misc/neopixel_testing/neopixel_testing.ino
@@ -2,27 +2,25 @@
 
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
-const unsigned LED_COUNT = 7;
-const uint8_t BRIGHTNESS = 255 * 0.4;
+const unsigned LED_COUNT =12;
+const uint8_t BRIGHTNESS = 255 * 0.15;
 
-const unsigned PIN_IN = 7;
-const unsigned PIN_OUT = 6;
+const unsigned PIN_OUT = 7;
+
+const unsigned BLINK_DELAY = 100;
 
 CRGB leds[LED_COUNT];
 
 void setup(){
 	FastLED.addLeds<LED_TYPE,PIN_OUT,COLOR_ORDER>(leds, LED_COUNT);
 	FastLED.setBrightness(BRIGHTNESS);
-	pinMode(PIN_IN, INPUT);
-	digitalWrite(PIN_IN, HIGH);
 }
 
 void loop(){
-	if(digitalRead(PIN_IN) == LOW){
-		fill_solid(leds, LED_COUNT, CRGB::Green);
-	} 
-	else {
-		fill_solid(leds, LED_COUNT, CRGB::Red);
-	}
+	fill_solid(leds, LED_COUNT, CRGB::Green);
 	FastLED.show();
+	delay(BLINK_DELAY);
+	FastLED.clear();
+	FastLED.show();
+	delay(BLINK_DELAY);
 }
